705d8a223c7446cf8d6ac9e272cdf5e6a86e5810
Archie Fan <archiefan@gmail.com>
2022-08-30 18:50:32 +0100

    Add project files.

---
+++ SignalRDemo/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRDemp
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        [HubMethodName("announcetoeverybody")]
+        public void Announce(string message)
+        {
+            Clients.All.Announce(message);
+        }
+
+        public DateTime GetServerDateTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ SignalRDemo/Scripts/signalr.js
@@ -0,0 +1,58 @@
+﻿//Immediately Invoked Function Expression
+
+(function () {
+
+    var chatHub = $.connection.chat;
+   
+    $.connection.hub.start()
+        .done(function () {
+            $.connection.hub.logging = true;
+            writeToPage("It works!");
+            $.connection.hub.log("Hub Connected!");
+            chatHub.server.announcetoeverybody("Connected!");
+            chatHub.server.getServerDateTime()
+                .done(function (data) {
+                    writeToPage(data);
+                })
+                .fail(function (e) {
+                    writeToPage(e);
+                });
+        })
+        .fail(function () {
+            writeToPage("ERROR connecting to SignalR!")
+        });
+
+    chatHub.client.announce = function (message) {
+        writeToPage(message);
+    }
+
+    var writeToPage = function (message) {
+        $("#welcome-message").append(message + "<br>");
+    }
+
+    $("#click-me").on("click", function () {
+        chatHub.server.getServerDateTime()
+            .done(function (data) {
+                writeToPage(data);
+            })
+            .fail(function (e) {
+                writeToPage(e);
+            });
+    });
+
+    //var x = 5;
+    //var myfunction = function () {
+    //    var x = 10;
+
+    //}
+    //myfunction();
+    //console.log(x); // return 5
+    //var myfunction = function () {
+    //    return 10;
+
+    //}
+    //x = myfunction();
+    //console.log(x); // return 10
+})()
+
+
